cdf28b3ad8fc420819a9ec67bf4f8cfec024badf
sasahara <zeal23310180@fsgcl.jp>
2024-05-14 13:52:49 +0900

    気にしないでください。

---
+++ 初級Java2.5/src/RectTest.java
@@ -0,0 +1,20 @@
+
+public class RectTest extends MyFrame{
+	public void run() {
+		setColor(255,255,0);
+		fillRect(30,40,50,100);
+		
+		setColor(200,0,255);
+		fillRect(130,40,50,100);
+		
+		setColor(128,200,255);
+		fillRect(230,40,50,100);
+		
+		setColor(0,0,0);
+		fillRect(330,40,50,100);
+		
+		setColor(100,100,100);
+		fillRect(40,200,100,40);
+		
+	}
+}
--- 初級java2-5/src/RectTest.java
+++
@@ -1,6 +0,0 @@
-
-public class RectTest extends MyFrame {
-public void run() {
-	fillRect(30,50,200,100);
-}
-}
