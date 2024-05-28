d34f9e9375cd09a9e65d53844eabe4a07b738920
sasahara <zeal23310180@fsgcl.jp>
2024-05-28 11:22:21 +0900

    練習問題をやった。

---
+++ 初級Java2.5/src/Car.java
@@ -0,0 +1,24 @@
+
+public class Car {
+	int x, y, vx, vy;
+
+	public Car(int x, int y, int vx, int vy) {
+		this.x = x;
+		this.y = y;
+		this.vx = vx;
+		this.vy = vy;
+	}
+
+	public void draw(MyFrame frame) {
+		frame.fillOval(x , y+30, 15, 15);
+		frame.fillOval(x+37, y+30, 15, 15);
+		
+		frame.fillRect(x+12, y , 30, 20);
+		frame.fillRect(x-3, y+10 , 60, 20);
+	}
+
+	public void move() {
+		x += vx;
+		y += vy;
+	}
+}
