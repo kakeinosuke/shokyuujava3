e5b9d22504c5a053306a345f338b495403b9a4a5
sasahara <zeal23310180@fsgcl.jp>
2024-06-11 09:59:08 +0900

    練習問題9-1をやった。

--- 初級Java2.5/初級Java/src/初級Java/Car.java
+++ 初級Java2.5/初級Java/src/初級Java/Car.java
@@ -37,13 +37,30 @@ public void keyPressed(KeyEvent e) {
 	if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
 		vx=5;
 	}
+		if(e.getKeyCode()==KeyEvent.VK_DOWN) {
+			vy=5;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_UP) {
+			vy=-5;
+	}
 }
 
 //キーが離されたとき
 @Override
 public void keyReleased(KeyEvent e) {
 	// TODO 自動生成されたメソッド・スタブ
-	vx=0;
+	if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+		vx=0;
+	}
+	if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+		vx=0;
+	}
+		if(e.getKeyCode()==KeyEvent.VK_DOWN) {
+			vy=0;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_UP) {
+			vy=0;
 }
 
+}
 }
